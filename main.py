157be916303a80c42c8ba0448c999b1333b576eb
Kierra McDowell <kmcdowe1@iwu.edu>
2022-12-09 21:05:52 -0600

    kierra's branch

--- main.py
+++ main.py
@@ -8,62 +8,55 @@ def reorder(store):
     try:
         cnx = mysql.connector.connect(user='JSKK', password='cs314', host='cs314.iwu.edu', database='jskk')
         cursor = cnx.cursor(buffered=True)
-        # check store_id if it's valid,  and put in a if statement to catch error if no store exists
         query = "SELECT * FROM store WHERE store_id = %s"
         params = (store,)
         cursor.execute(query, params)
-        store_name = cursor.fetchone()
-        if store_name is None:
-            print("Store does not exist")
-            return
-        store_inventory = "SELECT product_id, maximum_space, current_stock FROM inventory_space WHERE store_id = %s"
+        store_inventory = "SELECT * FROM inventory_space WHERE store_id = %s"
         cursor.execute(store_inventory, (store,))
-        store_items = cursor.fetchall()
-        for item in store_items:
-            item_price = "SELECT * FROM price WHERE product_id = %s"
-            cursor.execute(item_price, (item[0],))
+        store_items = cursor.fetchone()
+        item_price = "SELECT * FROM price WHERE product_id = %s"
         # This currently orders the same item over and over again
-        # should probably add loop to add several items 
-            price = cursor.fetchone()
-            vendor = price[0]
-            product_price = price[3]
-            current_stock = item[2]
-            max_stock = item[1]
-            if current_stock == max_stock:
-                print("There is no need to reorder")
-                return
-            else:
-                try:
-                    # Add a check to see if the request id is equivalent to the request we are working
-                    order_request = "SELECT * FROM order_request WHERE store_id = %s"
-                    cursor.execute(order_request)
-                    store_order_request = cursor.fetchall()
-
-                    total_amount = 0
-                    if store_order_request is None:
-                        print("There is no previous order request.")
-                    elif store_order_request is not None:
-                        for row in store_order_request:
-                            total_amount += row[4]
-                    if total_amount + current_stock < max_stock:
-                        print("Reorder is needed")
-                        reorder_amount = max_stock - current_stock - total_amount
-                        print("Reorder amount is:", reorder_amount)
-                        reorder_query = (
-                            "INSERT INTO order_request (vendor_id, store_id, amount_requested, order_time, "
-                            "total_cost, order_status,seen_or_not) VALUES (%s,%s, %s, %s, %s, %s, %s)")
-                        params = (vendor, store, reorder_amount, datetime.datetime.now(), product_price * reorder_amount, 0,0)
-                        cursor.execute(reorder_query, params)
-                        cnx.commit()
-                        return
-                    else:
-                        print("Order requests are already made")
-                        return
-                except:
-                    cnx.rollback()
-                    print("Something went wrong: {}")
+        # should probably add loop to add several different items 
+        cursor.execute(item_price, (store_items[0],))
+        price = cursor.fetchone()
+        vendor = price[0]
+        product_price = price[3]
+        current_stock = store_items[3]
+        max_stock = store_items[2]
+        if current_stock == max_stock:
+            print("There is no need to reorder")
+            return
+        else:
+            try:
+                # Add a check to see if the request id is equivalentt to the request we are working 
+                order_request = "SELECT * FROM order_request"
+                cursor.execute(order_request)
+                store_order_request = cursor.fetchall()
+                total_amount = 0
+                if store_order_request is None:
+                    print("There is no previous order request.")
+                elif store_order_request is not None:
+                    for row in store_order_request:
+                        total_amount += row[4]
+                if total_amount + current_stock < max_stock:
+                    print("Reorder is needed")
+                    reorder_amount = max_stock - current_stock - total_amount
+                    print("Reorder amount is:", reorder_amount)
+                    reorder_query = (
+                        "INSERT INTO order_request (vendor_id, store_id, amount_requested, order_time, "
+                        "total_cost, order_status,seen_or_not) VALUES (%s,%s, %s, %s, %s, %s, %s)")
+                    params = (vendor, store, reorder_amount, datetime.datetime.now(), product_price * reorder_amount, 0,0)
+                    cursor.execute(reorder_query, params)
+                    cnx.commit()
+                    return
+                else:
+                    print("Order requests are already made")
+                    return
+            except:
+                cnx.rollback()
+                print("Something went wrong: {}")
     except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
+        print("Something went wrong: {}")
         cnx.close()
 
 
@@ -90,17 +83,18 @@ def vendor_shipment(store, delivery_time, reorder_list, shipment_items):
         print(reorder_list)
 
         # Remaining reorder requests for this store particularly
-        orders_rem = "SELECT COUNT(store_id) FROM order_request WHERE store_id = %s AND order_status = 1"
+        orders_rem = "SELECT COUNT(store_id) FROM order_request WHERE store_id = %s AND order_status = 0"
         params5 = (store,)
         cursor.execute(orders_rem, params5)
         order_request = cursor.fetchone()
-        print("order_request[0]")
+        print(order_request[0])
 
 
         # Remaining reorder requests for all BMart stores
-    
-        total_price = 0
-        # can be used for reorder function
+        all_remaining_orders = "SELECT COUNT(request_id) FROM order_request order_status = 0"
+        cursor.execute(all_remaining_orders)
+        print(all_remaining_orders[0])
+        
         for reorder in reorder_list:
             # getting the count of each product id
             product_count = "SELECT amount_requested FROM order_request WHERE request_id = %s"
@@ -121,7 +115,7 @@ def vendor_shipment(store, delivery_time, reorder_list, shipment_items):
             #     params = (item,)
             #     cursor.execute(get_product, params)
             #     get_vendor = cursor.fetchone()
-          
+         
 
             # probably going to lose point because this is bad for longevity, use names instead 
             # stores unit price of vendor for each product_id
@@ -142,11 +136,39 @@ def vendor_shipment(store, delivery_time, reorder_list, shipment_items):
     else:
         cnx.close()
 
-#vendor_shipment(94, '2022-12-18', [483, 943, 31323], '30 mint oreos; 45 double stuff oreos; 90 regular oreos')
+vendor_shipment(94, '2022-12-18', [483, 943, 31323], '30 mint oreos; 45 double stuff oreos; 90 regular oreos')
 
 def stockInventory(store,shipment,shipment_items):
     try:
         cnx = mysql.connector.connect(user='JSKK', password='cs314', host='cs314.iwu.edu', database='jskk')
+        cursor = cnx.cursor(buffered=True)
+        # Details about the shipment that was just received, number of each product and product name that are supposed to be in shipment
+        product_list = "SELECT product_id, name, amount_requested FROM product JOIN order_group ON order_group.product_id = product.product_id"
+        product_list += "JOIN order_request ON order_group.request_id = order_request.product_id JOIN shipment_group on shipment_group.request_id = order_request.request_id"
+        product_list += "WHERE shipment_id = %s"
+        cursor.execute(product_list,(shipment,))
+        for (name, amount_requested) in cursor:
+            print({}, {}).format(amount_requested, name)
+
+        # Details about the shipment that was just received, number of each and product names that were actually received
+        unique_products = set[shipment_items]
+        for product in unique_products:
+            num_of_item = shipment_items.count(product)
+            product_name = "SELECT name FROM product WHERE product_id = %s"
+            cursor.execute(product_name, (product,))
+            for name in cursor:
+                print(num_of_item, "of", {}).format(name)
+
+        
+            # shipment_items will be a list of product ids 
+
+            # count the number of
+
+        # A list of the differences between what the shipment was supposed to contain and what it actually contained
+        
+
+
+       
 
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -182,4 +204,4 @@ def OnlineOrder(store,customer,order_items):
         cnx.close()
 
 
-reorder(2)
+reorder(1)
